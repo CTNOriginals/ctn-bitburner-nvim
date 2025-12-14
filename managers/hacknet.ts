d0532dd26f0f57f003093a5112a4698ca9f16bcc
CTNOriginals <info@ctn-originals.com>
2025-12-14 09:20:04 +0100

    feat: Compleated hacknet manager functionality

--- managers/hacknet.ts
+++ managers/hacknet.ts
@@ -1,3 +1,68 @@
+export async function main(ns: NS) {
+	ns.tprint("Hacking some nets!")
+
+	const nodes: Node[] = []
+	let numNodes = ns.hacknet.numNodes()
+
+	for (let i = 0; i < numNodes; i++) {
+		nodes.push(new Node(ns, i))
+	}
+
+	while (true) {
+		// sleep first to account for the chance of the loop continueing in the middle
+		await ns.sleep(100)
+
+		let bestComp = getCheapestUpgrade(nodes)
+
+		if (!bestComp) {
+			continue
+		}
+
+		if (ns.getPlayer().money > bestComp.GetCost()) {
+			const i = bestComp.Upgrade()
+
+			if (bestComp.Typ == ENode.node) {
+				nodes.push(new Node(ns, i as number))
+			}
+
+			ns.tprint(bestComp.String())
+		}
+	}
+}
+
+function getCheapestUpgrade(nodes: Node[]): NodeComponent | null {
+	let bestVal = -1
+	let bestComp: NodeComponent = nodes[0].Components[1]
+
+	for (const node of nodes) {
+		// ns.tprint(node.StringValue(' '))
+
+		let lowestVal: number = -1
+		let lowestComp = node.Components[0]
+
+		for (const compName in node.Components) {
+			let comp: NodeComponent = node.Components[compName]
+
+			if (comp.IsMax()) {
+				continue
+			}
+
+			let cost = comp.GetCost()
+
+			if (lowestVal > cost || lowestVal < 0) {
+				lowestComp = comp
+				lowestVal = cost
+			}
+		}
+
+		if (bestVal > lowestVal || bestVal < 0) {
+			bestVal = lowestVal
+			bestComp = lowestComp
+		}
+	}
+
+	return bestComp
+}
 
 const HacknetNodeConstants = {
 	MoneyGainPerLevel: 1.5,
@@ -27,45 +92,58 @@ const ENode = {
 type TENode = keyof typeof ENode
 
 class NodeComponent {
-	public GetCost: (i: number, n?: number) => number
-	public Upgrade: (i: number, n?: number) => number | boolean
 	public MaxValue: number
 
-	constructor(private ns: NS, public Typ: TENode) {
+	private getCost: (i: number, n?: number) => number
+	private upgrade: (i: number, n?: number) => number | boolean
+
+	constructor(private ns: NS, public Index: number, public Typ: TENode) {
 		switch (Typ) {
 			case ENode.node: {
-				this.GetCost = ns.hacknet.getPurchaseNodeCost
-				this.Upgrade = ns.hacknet.purchaseNode
+				this.getCost = ns.hacknet.getPurchaseNodeCost
+				this.upgrade = ns.hacknet.purchaseNode
 				this.MaxValue = ns.hacknet.maxNumNodes()
 			} break;
 			case ENode.level: {
-				this.GetCost = ns.hacknet.getLevelUpgradeCost
-				this.Upgrade = ns.hacknet.upgradeLevel
+				this.getCost = ns.hacknet.getLevelUpgradeCost
+				this.upgrade = ns.hacknet.upgradeLevel
 				this.MaxValue = HacknetNodeConstants.MaxLevel
 			} break;
 			case ENode.ram: {
-				this.GetCost = ns.hacknet.getRamUpgradeCost
-				this.Upgrade = ns.hacknet.upgradeRam
+				this.getCost = ns.hacknet.getRamUpgradeCost
+				this.upgrade = ns.hacknet.upgradeRam
 				this.MaxValue = HacknetNodeConstants.MaxRam
 			} break;
 			case ENode.cores: {
-				this.GetCost = ns.hacknet.getCoreUpgradeCost
-				this.Upgrade = ns.hacknet.upgradeCore
+				this.getCost = ns.hacknet.getCoreUpgradeCost
+				this.upgrade = ns.hacknet.upgradeCore
 				this.MaxValue = HacknetNodeConstants.MaxCores
 			} break;
 		}
 	}
 
-	public GetValue(i: number): number {
-		if (this.Typ == ENode.node) {
-			return i
-		}
-
-		return this.ns.hacknet.getNodeStats(i)[this.Typ]
+	public GetCost(n: number = 1): number {
+		return this.getCost(this.Index, n)
 	}
 
-	public IsMax(i: number): boolean {
-		return this.GetValue(i) >= this.MaxValue
+	public Upgrade(n: number = 1): boolean | number {
+		return this.upgrade(this.Index, n)
+	}
+
+	public GetValue(): number {
+		if (this.Typ == ENode.node) {
+			return this.Index
+		}
+
+		return this.ns.hacknet.getNodeStats(this.Index)[this.Typ]
+	}
+
+	public IsMax(): boolean {
+		return this.GetValue() >= this.MaxValue
+	}
+
+	public String() {
+		return `${this.Index}[${this.Typ}]: value:${this.GetValue()} cost:${this.GetCost()} max:${this.IsMax()}`
 	}
 }
 
@@ -74,10 +152,10 @@ class Node {
 
 	constructor(ns: NS, public Index: number) {
 		this.Components = {
-			[ENode.node]: new NodeComponent(ns, ENode.node),
-			[ENode.level]: new NodeComponent(ns, ENode.level),
-			[ENode.ram]: new NodeComponent(ns, ENode.ram),
-			[ENode.cores]: new NodeComponent(ns, ENode.cores),
+			[ENode.node]: new NodeComponent(ns, this.Index, ENode.node),
+			[ENode.level]: new NodeComponent(ns, this.Index, ENode.level),
+			[ENode.ram]: new NodeComponent(ns, this.Index, ENode.ram),
+			[ENode.cores]: new NodeComponent(ns, this.Index, ENode.cores),
 		}
 	}
 
@@ -87,30 +165,16 @@ class Node {
 		for (const name in this.Components) {
 			const comp = this.Components[name as TENode]
 
-			lines.push(`${name}:${comp.GetValue(this.Index)}`)
+			lines.push(`${name}:${comp.GetValue()}`)
 		}
 
 		return lines.join(seperator)
 	}
+
+
 }
 
 
-export function main(ns: NS) {
-	ns.tprint("Hacking some nets!")
-
-	const nodes: Node[] = []
-	let numNodes = ns.hacknet.numNodes()
-
-	for (let i = 0; i < numNodes; i++) {
-		let stats = ns.hacknet.getNodeStats(i)
-
-		nodes.push(new Node(ns, i))
-	}
-
-	for (const node of nodes) {
-		ns.tprint(node.StringValue(' '))
-	}
-}
 
 
 
