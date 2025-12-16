e543c2a25c83d16ca1d4f149b7fdc5e7ef4bf32c
CTNOriginals <info@ctn-originals.com>
2025-12-16 21:06:50 +0100

    MISC: Add some needed global refs

--- globals.d.ts
+++ globals.d.ts
@@ -1,5 +1,10 @@
-import {NS as _NS} from './NetscriptDefinitions';
+import type * as _NSD from './NetscriptDefinitions';
 
 declare global {
-	type NS = _NS;
+	type NS = _NSD.NS;
+	type ProcessInfo = _NSD.ProcessInfo
+	type RunningScript = _NSD.RunningScript
+	type RecentScript = _NSD.RecentScript
+	type ScriptArg = _NSD.ScriptArg
 }
+
