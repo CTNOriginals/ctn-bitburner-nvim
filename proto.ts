c2473a608f49db2e739ceb75acb68c717b0204a3
CTNOriginals <info@ctn-originals.com>
2025-12-18 11:39:47 +0100

    FEAT: Watcher functional

--- proto.ts
+++ proto.ts
@@ -1,17 +1,61 @@
+let percist = 0
 
 export async function main(ns: NS) {
-	ns.tprint('Prototype')
-	// ns.run('managers/hacknet.ts!')
-	ns.tprint(ns.getMoneySources().sinceInstall.hacknet)
-	ns.tprint(ns.getMoneySources().sinceInstall.hacknet_expenses)
-	ns.tprint(ns.getMoneySources().sinceInstall.hacknet - ns.getMoneySources().sinceInstall.hacknet_expenses)
-	while (true) {
-		await ns.sleep(1000)
-	}
+	ns.disableLog('ALL')
+
+	ns.print(percist)
+	await ns.sleep(4000)
+	percist++
+
+	// while (true) {
+	// 	await ns.sleep(1000)
+	// 	const testScript = getTestScript(ns)
+	//
+	// 	if (!testScript) {
+	// 		continue
+	// 	}
+	//
+	// 	let now = new Date().getTime()
+	//
+	// 	let mtime = ns.getFileMetadata(testScript.filename).mtime
+	// 	let msec = (now - mtime) / 1000
+	// 	let runtime = testScript.onlineRunningTime
+	//
+	// 	ns.print(`script  : ${testScript.pid} ${testScript.filename} ${testScript.args}`)
+	// 	ns.print(`\nmod time: ${mtime}`)
+	// 	ns.print(`mod sec : ${msec}`)
+	//
+	// 	if ('timeOfDeath' in testScript) {
+	// 		runtime = now - testScript.endTime
+	// 		runtime /= 1000
+	// 	}
+	//
+	// 	ns.print('\n')
+	// 	ns.print(`${('timeOfDeath' in testScript) ? 'death  ' : 'runtime'} : ${runtime}`)
+	// 	ns.print(`diff    : ${msec - runtime}`)
+	//
+	// 	ns.print('\n\n')
+	// }
 }
 
+type TTestScript = RunningScript | RecentScript | null
+function getTestScript(ns: NS): TTestScript {
+	let testScript: TTestScript = null
+	for (const script of ns.ps()) {
+		if (!script.args.includes('--watch')) {
+			continue
+		}
 
+		return ns.getRunningScript(script.pid)!
+	}
 
+	for (const script of ns.getRecentScripts().reverse()) {
+		if (!script.args.includes('--watch')) {
+			continue
+		}
+		return script
+	}
 
-
+	return null
+}
 
--- tools/watcher/index.ts
+++ tools/watcher/index.ts
@@ -1,17 +1,31 @@
+
 type TScriptMap = { [pid: number]: RunningScript }
+type TFileModTime = { [file: string]: number }
 
-const updateInterval = 1000
 
-interface IHasPid {
-	pid: number,
-	args: ScriptArg[]
+export async function main(ns: NS) {
+	ns.disableLog('ALL')
+	// ns.clearLog()
+
+	const updateInterval = 1000
+
+	while (true) {
+		await ns.sleep(updateInterval)
+		const WatchMap = getWatchMap(ns)
+
+		for (const pid in WatchMap) {
+			const script = WatchMap[pid]
+			const modTime = ns.getFileMetadata(script.filename).mtime
+
+			if (script.startTime < modTime) {
+				restartScript(ns, script)
+			}
+		}
+	}
 }
 
-export function main(ns: NS) {
-	ns.disableLog('ALL')
+function getWatchMap(ns: NS): TScriptMap {
 	const WatchMap: TScriptMap = {}
-	const scripts: IHasPid[] = [...ns.ps(), ...ns.getRecentScripts()]
-
 	for (const ps of ns.ps()) {
 		if (!ps.args.includes('--watch')) {
 			continue
@@ -26,9 +40,15 @@ export function main(ns: NS) {
 	}
 
 	for (const recent of ns.getRecentScripts()) {
+		// if (recent.filename == ns.getScriptName()) {
+		// 	ns.tprint('ignoring recent watcher')
+		// 	continue
+		// }
+
 		if (
 			!recent.args.includes('--watch')
 			|| recent.tailProperties === null
+			|| Object.keys(WatchMap).includes(recent.pid.toString())
 		) {
 			continue
 		}
@@ -36,15 +56,44 @@ export function main(ns: NS) {
 		WatchMap[recent.pid] = recent
 	}
 
-	for (const pid in WatchMap) {
-		const script = WatchMap[pid]
-		ns.print(`${pid}: ${script.filename} ${script.args} = ${script.tailProperties !== null}`)
+	return WatchMap
+}
+
+function restartScript(ns: NS, script: RunningScript) {
+	ns.print(`Restarting script: ${script.pid} ${script.filename}`)
+	if (ns.isRunning(script.pid)) {
+		if (script.pid == ns.pid) {
+			restartSelf(ns, script)
+			return
+		} else {
+			ns.kill(script.pid)
+		}
 	}
+
+	const pid = ns.run(script.filename, undefined, ...script.args)
+	if (pid == 0) {
+		ns.print(`Failed to run script: ${script.filename} ${script.args}`)
+		return
+	}
+
+	if (script.tailProperties === null) {
+		return
+	}
+
+	ns.ui.closeTail(script.pid)
+	ns.ui.openTail(pid)
+	ns.ui.resizeTail(script.tailProperties.width, script.tailProperties.height, pid)
+	ns.ui.moveTail(script.tailProperties.x, script.tailProperties.y, pid)
 }
 
-async function update(ns: NS) {
-	// ns.getRecentScripts
+function restartSelf(ns: NS, script: RunningScript) {
+	ns.tprint(`Killing ${ns.pid} ${script.filename} ${script.args}`)
+	if (script.tailProperties === null) {
+		// script.args.push('--tail')
+		// ns.ui.closeTail()
+	}
 
-
+	ns.spawn(script.filename, { spawnDelay: 0, preventDuplicates: true }, ...script.args)
 }
+
 
