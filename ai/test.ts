ddf438699eada1bc99df39e4f83e3efb52b194b6
CTNOriginals <info@ctn-originals.com>
2025-12-23 21:14:57 +0100

    MISC: Move XOR AI to its own model

--- ai/test.ts
+++ ai/test.ts
@@ -6,30 +6,6 @@ import * as AdderTest from './models/adder/test.ts'
 
 import { Logger } from "../logging/index.ts";
 
-// @ts-expect-error: ts2310 circular reference
-// The error was that AAIDef<AIXOR> is a circular ref
-// which is true and is bad, but it doesnt actually break
-// and the payoff for the types functioning like this is way greater
-// so shut the fuck up TS
-class AIXOR extends AAIDef<AIXOR> {
-	private io = CIODef.define(0, 1)
-
-	public Inputs = {
-		x: this.io,
-		y: this.io,
-	} as const
-	public Outputs = {
-		out: this.io,
-	} as const
-
-	protected HiddenLayers: number[] = [3, 5, 3]
-
-	constructor() {
-		super()
-		super.createNeuralNetwork()
-	}
-}
-
 let logger: Logger
 let thisNS: NS
 
@@ -42,55 +18,5 @@ export async function main(ns: NS) {
 	logger = new Logger(ns)
 
 	AdderTest.main(ns)
-
-	// const ai = new AIXOR()
-	//
-	// logger.log(ai.Inputs)
-	// logger.log(ai.Outputs)
-	// ai.Train([
-	// 	{
-	// 		inputs: { x: 0, y: 0 },
-	// 		outputs: { out: 0 }
-	// 	},
-	// 	{
-	// 		inputs: { x: 1, y: 0 },
-	// 		outputs: { out: 1 }
-	// 	},
-	// 	{
-	// 		inputs: { x: 0, y: 1 },
-	// 		outputs: { out: 1 }
-	// 	},
-	// 	{
-	// 		inputs: { x: 1, y: 1 },
-	// 		outputs: { out: 0 }
-	// 	},
-	// ], 10000, 0.5)
-	//
-	// logger.log('00: ', ai.Test({ x: 0, y: 0 })['out'])
-	// logger.log('01: ', ai.Test({ x: 0, y: 1 })['out'])
-	// logger.log('10: ', ai.Test({ x: 1, y: 0 })['out'])
-	// logger.log('11: ', ai.Test({ x: 1, y: 1 })['out'])
-	//
-	// logger.log(ai.GetInputValues({ x: 1, y: 0 }))
-	// logger.log(ai.GetInputVariants([0, 1]))
-	// logger.log(ai.GetOutputVariants([1]))
-	// logger.log(ai.InputKeys)
-}
-
-// Old way of controlling a neural network
-// without the abstract AI definitions
-function xorSolver(ns: NS) {
-	const nn = new NeuralNetwork([2, 4, 4, 1]);
-	const inputs = [[0, 0], [0, 1], [1, 0], [1, 1]];
-	const targets = [[0], [1], [1], [0]];
-
-	// console.log(targets)
-	nn.train(inputs, targets, 10000, 0.1);
-
-	inputs.forEach((input, i) => {
-		ns.print(`Input: ${input}, Target: ${targets[i]}, Predicted: ${nn.forward(input)}`)
-	})
-
-	ns.write('tmp.json', JSON.stringify(nn.layers, null, 2), 'w');
 }
 
