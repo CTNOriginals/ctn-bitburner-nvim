08f56ccbd6009d6ab39150d866713aa153383260
CTNOriginals <info@ctn-originals.com>
2025-12-29 20:18:18 +0100

    MISC: Cleanup batcher controller

--- hack/batcher/controller.ts
+++ hack/batcher/controller.ts
@@ -1,17 +1,50 @@
 import { Logger } from '../../logging/index.ts'
 import * as Worm from '../worm.ts'
 
-class ServerBatcher {
-
-	constructor(private ns: NS, public Host: string) { }
-
-	public GetServer(): Server {
-		return this.ns.getServer(this.Host)
-	}
-}
-
 type TServer = Required<Server>
 type THostMap<T> = { [host: string]: T }
+
+class ServerController {
+	constructor(
+		private ns: NS,
+		public Host: string,
+	) { }
+
+	public GetServer(): TServer {
+		return this.ns.getServer(this.Host) as TServer
+	}
+
+	public GetTotalTime(): number {
+		const server = this.GetServer()
+		return this.ns.getWeakenTime(server.hostname)
+			+ this.ns.getGrowTime(server.hostname)
+			+ this.ns.getHackTime(server.hostname)
+	}
+
+	public GetScore(): number {
+		const server = this.GetServer()
+		const growth = server.serverGrowth
+		const max = server.moneyMax
+		const time = this.GetTotalTime()
+
+		return (max / (100 - growth)) / time
+	}
+
+	public GetInfoString(): string {
+		const nf = this.ns.format.number
+		return [
+			`${this.Host}:${" ".repeat(18 - this.Host.length)} `,
+			`${nf(this.GetServer().moneyMax)}`,
+			`\t${this.GetServer().serverGrowth}`,
+			`\t${Math.round(this.GetTotalTime() / 1000)}`,
+			`\t= ${nf(this.GetScore(), 2)}`,
+		].join('')
+	}
+
+	public Initialize() {
+
+	}
+}
 
 export async function main(ns: NS) {
 	ns.disableLog('ALL')
@@ -21,60 +54,43 @@ export async function main(ns: NS) {
 
 	const host = ns.getHostname()
 
-	const servers: THostMap<TServer> = {}
+	const controllers: THostMap<ServerController> = {}
 	const hostList: string[] = []
 
 	await Worm.ServerWorm(ns, host, (server: Server) => {
 		// servers.push(server as TServer)
-		servers[server.hostname] = server as TServer
+		controllers[server.hostname] = new ServerController(ns, server.hostname)
 		hostList.push(server.hostname)
 	})
 
-	const totalTime = (server: TServer): number => {
-		return ns.getWeakenTime(server.hostname) + ns.getGrowTime(server.hostname) + ns.getHackTime(server.hostname)
-	}
-	const serverScore = (server: TServer): number => {
-		const growth = server.serverGrowth
-		const max = server.moneyMax
-		const time = totalTime(server)
-		// const x = server.hackDifficulty
-
-		return (max / (100 - growth)) / time
-	}
-
-	hostList.sort((a, b) => { return serverScore(servers[b]) - serverScore(servers[a]) })
+	hostList.sort((a, b) => { return controllers[b].GetScore() - controllers[a].GetScore() })
 
 	const maxspace = 18
 	let totalScore = 0
 	const serverScores: { [host: string]: number } = {}
 
-	for (const host in servers) {
-		const server = servers[host]
-		const score = serverScore(server)
+	for (const host in controllers) {
+		const server = controllers[host]
+		const score = server.GetScore()
 
-		if (server.serverGrowth <= 1 || score <= 0) {
+		if (server.GetServer().serverGrowth <= 1 || score <= 0) {
 			continue
 		}
 
-		serverScores[server.hostname] = score
+		serverScores[server.GetServer().hostname] = score
 		totalScore += score
 	}
 
 	for (const host of hostList) {
+		const server = controllers[host]
 		const score = serverScores[host]
+
 		if (!score) {
 			continue
 		}
-		const server = servers[host]
-		const space = maxspace - host.length
 
 		log([
-			`${host}:${" ".repeat(space)} `,
-			`${nf(server.moneyMax)}`,
-			`\t${100 - server.serverGrowth}`,
-			`\t${Math.round(totalTime(server) / 1000)}`,
-			`\t= ${nf(score, 2)}`,
-			// ` \t(${Math.round(((score / totalScore) * 100) * 100) / 100})`,
+			`${server.GetInfoString()}`,
 			` \t(${ns.format.percent(((score / totalScore)))})`,
 		].join(''))
 	}
