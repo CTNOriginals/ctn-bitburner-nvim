74628030bea5f71bf1b666920c493b30fa18d24c
CTNOriginals <info@ctn-originals.com>
2025-12-30 14:22:37 +0100

    IMPORT: RunInBackground.js by d0sboots

---
+++ tools/RunInBackground.js
@@ -0,0 +1,423 @@
+/* Apache License
+	   Version 2.0, January 2004
+	http://www.apache.org/licenses/
+
+	TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+
+	1. Definitions.
+
+		  "License" shall mean the terms and conditions for use, reproduction,
+		  and distribution as defined by Sections 1 through 9 of this document.
+
+		  "Licensor" shall mean the copyright owner or entity authorized by
+		  the copyright owner that is granting the License.
+
+		  "Legal Entity" shall mean the union of the acting entity and all
+		  other entities that control, are controlled by, or are under common
+		  control with that entity. For the purposes of this definition,
+		  "control" means (i) the power, direct or indirect, to cause the
+		  direction or management of such entity, whether by contract or
+		  otherwise, or (ii) ownership of fifty percent (50%) or more of the
+		  outstanding shares, or (iii) beneficial ownership of such entity.
+
+		  "You" (or "Your") shall mean an individual or Legal Entity
+		  exercising permissions granted by this License.
+
+		  "Source" form shall mean the preferred form for making modifications,
+		  including but not limited to software source code, documentation
+		  source, and configuration files.
+
+		  "Object" form shall mean any form resulting from mechanical
+		  transformation or translation of a Source form, including but
+		  not limited to compiled object code, generated documentation,
+		  and conversions to other media types.
+
+		  "Work" shall mean the work of authorship, whether in Source or
+		  Object form, made available under the License, as indicated by a
+		  copyright notice that is included in or attached to the work
+		  (an example is provided in the Appendix below).
+
+		  "Derivative Works" shall mean any work, whether in Source or Object
+		  form, that is based on (or derived from) the Work and for which the
+		  editorial revisions, annotations, elaborations, or other modifications
+		  represent, as a whole, an original work of authorship. For the purposes
+		  of this License, Derivative Works shall not include works that remain
+		  separable from, or merely link (or bind by name) to the interfaces of,
+		  the Work and Derivative Works thereof.
+
+		  "Contribution" shall mean any work of authorship, including
+		  the original version of the Work and any modifications or additions
+		  to that Work or Derivative Works thereof, that is intentionally
+		  submitted to Licensor for inclusion in the Work by the copyright owner
+		  or by an individual or Legal Entity authorized to submit on behalf of
+		  the copyright owner. For the purposes of this definition, "submitted"
+		  means any form of electronic, verbal, or written communication sent
+		  to the Licensor or its representatives, including but not limited to
+		  communication on electronic mailing lists, source code control systems,
+		  and issue tracking systems that are managed by, or on behalf of, the
+		  Licensor for the purpose of discussing and improving the Work, but
+		  excluding communication that is conspicuously marked or otherwise
+		  designated in writing by the copyright owner as "Not a Contribution."
+
+		  "Contributor" shall mean Licensor and any individual or Legal Entity
+		  on behalf of whom a Contribution has been received by Licensor and
+		  subsequently incorporated within the Work.
+
+	   2. Grant of Copyright License. Subject to the terms and conditions of
+		  this License, each Contributor hereby grants to You a perpetual,
+		  worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+		  copyright license to reproduce, prepare Derivative Works of,
+		  publicly display, publicly perform, sublicense, and distribute the
+		  Work and such Derivative Works in Source or Object form.
+
+	   3. Grant of Patent License. Subject to the terms and conditions of
+		  this License, each Contributor hereby grants to You a perpetual,
+		  worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+		  (except as stated in this section) patent license to make, have made,
+		  use, offer to sell, sell, import, and otherwise transfer the Work,
+		  where such license applies only to those patent claims licensable
+		  by such Contributor that are necessarily infringed by their
+		  Contribution(s) alone or by combination of their Contribution(s)
+		  with the Work to which such Contribution(s) was submitted. If You
+		  institute patent litigation against any entity (including a
+		  cross-claim or counterclaim in a lawsuit) alleging that the Work
+		  or a Contribution incorporated within the Work constitutes direct
+		  or contributory patent infringement, then any patent licenses
+		  granted to You under this License for that Work shall terminate
+		  as of the date such litigation is filed.
+
+	   4. Redistribution. You may reproduce and distribute copies of the
+		  Work or Derivative Works thereof in any medium, with or without
+		  modifications, and in Source or Object form, provided that You
+		  meet the following conditions:
+
+		  (a) You must give any other recipients of the Work or
+			  Derivative Works a copy of this License; and
+
+		  (b) You must cause any modified files to carry prominent notices
+			  stating that You changed the files; and
+
+		  (c) You must retain, in the Source form of any Derivative Works
+			  that You distribute, all copyright, patent, trademark, and
+			  attribution notices from the Source form of the Work,
+			  excluding those notices that do not pertain to any part of
+			  the Derivative Works; and
+
+		  (d) If the Work includes a "NOTICE" text file as part of its
+			  distribution, then any Derivative Works that You distribute must
+			  include a readable copy of the attribution notices contained
+			  within such NOTICE file, excluding those notices that do not
+			  pertain to any part of the Derivative Works, in at least one
+			  of the following places: within a NOTICE text file distributed
+			  as part of the Derivative Works; within the Source form or
+			  documentation, if provided along with the Derivative Works; or,
+			  within a display generated by the Derivative Works, if and
+			  wherever such third-party notices normally appear. The contents
+			  of the NOTICE file are for informational purposes only and
+			  do not modify the License. You may add Your own attribution
+			  notices within Derivative Works that You distribute, alongside
+			  or as an addendum to the NOTICE text from the Work, provided
+			  that such additional attribution notices cannot be construed
+			  as modifying the License.
+
+		  You may add Your own copyright statement to Your modifications and
+		  may provide additional or different license terms and conditions
+		  for use, reproduction, or distribution of Your modifications, or
+		  for any such Derivative Works as a whole, provided Your use,
+		  reproduction, and distribution of the Work otherwise complies with
+		  the conditions stated in this License.
+
+	   5. Submission of Contributions. Unless You explicitly state otherwise,
+		  any Contribution intentionally submitted for inclusion in the Work
+		  by You to the Licensor shall be under the terms and conditions of
+		  this License, without any additional terms or conditions.
+		  Notwithstanding the above, nothing herein shall supersede or modify
+		  the terms of any separate license agreement you may have executed
+		  with Licensor regarding such Contributions.
+
+	   6. Trademarks. This License does not grant permission to use the trade
+		  names, trademarks, service marks, or product names of the Licensor,
+		  except as required for reasonable and customary use in describing the
+		  origin of the Work and reproducing the content of the NOTICE file.
+
+	   7. Disclaimer of Warranty. Unless required by applicable law or
+		  agreed to in writing, Licensor provides the Work (and each
+		  Contributor provides its Contributions) on an "AS IS" BASIS,
+		  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+		  implied, including, without limitation, any warranties or conditions
+		  of TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A
+		  PARTICULAR PURPOSE. You are solely responsible for determining the
+		  appropriateness of using or redistributing the Work and assume any
+		  risks associated with Your exercise of permissions under this License.
+
+	   8. Limitation of Liability. In no event and under no legal theory,
+		  whether in tort (including negligence), contract, or otherwise,
+		  unless required by applicable law (such as deliberate and grossly
+		  negligent acts) or agreed to in writing, shall any Contributor be
+		  liable to You for damages, including any direct, indirect, special,
+		  incidental, or consequential damages of any character arising as a
+		  result of this License or out of the use or inability to use the
+		  Work (including but not limited to damages for loss of goodwill,
+		  work stoppage, computer failure or malfunction, or any and all
+		  other commercial damages or losses), even if such Contributor
+		  has been advised of the possibility of such damages.
+
+	   9. Accepting Warranty or Additional Liability. While redistributing
+		  the Work or Derivative Works thereof, You may choose to offer,
+		  and charge a fee for, acceptance of support, warranty, indemnity,
+		  or other liability obligations and/or rights consistent with this
+		  License. However, in accepting such obligations, You may act only
+		  on Your own behalf and on Your sole responsibility, not on behalf
+		  of any other Contributor, and only if You agree to indemnify,
+		  defend, and hold each Contributor harmless for any liability
+		  incurred by, or claims asserted against, such Contributor by reason
+		  of your accepting any such warranty or additional liability.
+
+	   END OF TERMS AND CONDITIONS
+
+	   APPENDIX: How to apply the Apache License to your work.
+
+		  To apply the Apache License to your work, attach the following
+		  boilerplate notice, with the fields enclosed by brackets "[]"
+		  replaced with your own identifying information. (Don't include
+		  the brackets!)  The text should be enclosed in the appropriate
+		  comment syntax for the file format. We also recommend that a
+		  file or class name and description of purpose be included on the
+		  same "printed page" as the copyright notice for easier
+		  identification within third-party archives.
+
+	   Copyright [yyyy] [name of copyright owner]
+
+	   Licensed under the Apache License, Version 2.0 (the "License");
+	   you may not use this file except in compliance with the License.
+	   You may obtain a copy of the License at
+
+		   http://www.apache.org/licenses/LICENSE-2.0
+
+	   Unless required by applicable law or agreed to in writing, software
+	   distributed under the License is distributed on an "AS IS" BASIS,
+	   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	   See the License for the specific language governing permissions and
+	   limitations under the License.
+*/
+
+class RunInBackground {
+	/**
+	 * Current AudioContext that is running audio. null when not running.
+	 * @type {(AudioContext | null)}
+	 */
+	ctx = null;
+	/**
+	 * Whether the class is active, i.e. should be playing audio. Defaults to
+	 * true, because that's what's expected. We persist the value of the setting
+	 * by *modifying the default value* here, via self-modifying code.
+	 * @type {boolean}
+	 */
+	active = true;
+	/**
+	 * Reference to the OptionSwitch function component, hooked from the game.
+	 * @type {(OptionSwitch | null)}
+	 */
+	OptionSwitch = null; /* OptionSwitch | null */
+	/**
+	 * The workerScript object. This is what backs "ns", and having direct
+	 * access gives us incredible power: The ability to make calls without RAM
+	 * usage, call internal functions, and much more. We're only using it to
+	 * make log entries and re-write our code after the script has "died".
+	 * @type {(WorkerScript | null)}
+	 */
+	workerScript = null;
+
+	/**
+	 * Plays a constant hum (too slow and low to be heard) to prevent backgrounding the tab.
+	 * @param {Event} evt
+	 */
+	updateHum(evt) {
+		// The exact equality means that no event = run always
+		if (evt?.isTrusted === false) return;
+		if (this.active === !!this.ctx) return;
+		if (this.ctx) {
+			this.ctx.close();
+			this.ctx = null;
+		} else {
+			this.ctx = new AudioContext({ latencyHint: "playback" });
+			const osc = this.ctx.createOscillator();
+			// 1Hz - far too low to be audible
+			osc.frequency.setValueAtTime(1, this.ctx.currentTime);
+			const gain = this.ctx.createGain();
+			// This is just above the threshold where playback is considered "silent".
+			gain.gain.setValueAtTime(0.001, this.ctx.currentTime);
+			// Have to avoid picking up the RAM cost of singularity.connect
+			osc["connect"](gain);
+			gain["connect"](this.ctx.destination);
+			osc.start();
+		}
+	}
+
+	/** @returns React.Element to be added to the options menu */
+	createOption() {
+		if (!this.OptionSwitch) {
+			return React.createElement(
+				"div",
+				{
+					style: {
+						color: "red",
+						fontSize: "30px",
+						fontFamily: "sans-serif",
+					},
+				},
+				`Error initializing ${this.workerScript.name}`
+			);
+		} else {
+			return React.createElement(this.OptionSwitch, {
+				checked: this.active,
+				onChange: (newValue) => {
+					if (this.active !== newValue) {
+						this.active = newValue;
+						this.writeDefaultValue();
+					}
+					this.updateHum();
+				},
+				text: "Run in background (unneeded for Steam version)",
+				tooltip: `If this is set, the game will keep running even when it doesn't have focus.
+          This uses the audio API, so you may see an audio indicator for the tab but it shouldn't
+          make noise. If unset and running in a browser, the game will process slowly when the
+          tab isn't focused. The Steam version always runs, even in the background.`,
+			});
+		}
+	}
+
+	writeDefaultValue() {
+		this.workerScript.print(`Option toggled to ${this.active}`);
+		// Have to avoid picking this up as an NS function
+		const server = this.workerScript["getServer"]();
+		const script = server.scripts.get(this.workerScript.name);
+		if (!script) {
+			this.workerScript.print(
+				`Couldn't find script for ${this.workerScript.name}!`
+			);
+			return;
+		}
+		// Match the line where we set the default value, and capture the value
+		// itself, with indices. This is specific enough to not match anything else.
+		const pat = /^ *active = ([a-z]*);/dm;
+		// Avoid parsing as ns.exec()
+		const result = pat["exec"](script.content);
+		if (!result) {
+			this.workerScript.print(
+				`Couldn't match pattern to rewrite the default value in ${this.workerScript.name}!`
+			);
+			return;
+		}
+		script.content =
+			script.content.slice(0, result.indices[1][0]) +
+			String(this.active) +
+			script.content.slice(result.indices[1][1]);
+	}
+
+	registerHandler() {
+		// We need to call updateHum from a user action, because Chrome won't give
+		// us a working AudioContext except in that circumstance. Clicking on the
+		// settings toggle counts, but we need this listener to kick-start things
+		// at game start.
+		// The options mean the listener is in the "capturing" phase (before other
+		// listeners can cancel event propogation for us), and "passive" because
+		// we never preventDefault().
+		globalThis["document"].addEventListener(
+			"click",
+			this.updateHum.bind(this),
+			{ capture: true, passive: true }
+		);
+	}
+
+	hookReact() {
+		const orig = React.createElement;
+		const _this = this;
+		React.createElement = function(...args) {
+			const fn = args[0];
+			const props = args[1];
+			if (
+				typeof fn !== "function" ||
+				props === null ||
+				typeof props !== "object" ||
+				props.title !== "System" ||
+				!String(fn).includes('height:"fit-content"')
+			) {
+				return orig.call(this, ...args);
+			}
+			if (_this.OptionSwitch === null) {
+				let i = 2;
+				for (; i < args.length; ++i) {
+					const child = args[i];
+					if (
+						child !== null &&
+						typeof child === "object" &&
+						typeof child.type === "function" &&
+						String(child.type).includes(".target.checked")
+					) {
+						_this.OptionSwitch = child.type;
+						break;
+					}
+				}
+				if (i >= args.length) {
+					_this.workerScript.print("Unable to find OptionSwitch!");
+				}
+			}
+			// Add our option to the end of the children
+			return orig.call(this, ...args, _this.createOption());
+		};
+		return orig;
+	}
+
+	/** @param {NS} ns */
+	hookWorkerScript(ns) {
+		const orig = Map.prototype.get;
+		const _this = this;
+		Map.prototype.get = function(...args) {
+			// We can safely assume that the first call to this will be the call we
+			// want, because of synchronous execution.
+			Map.prototype.get = orig;
+			_this.workerScript = orig.call(this, ...args);
+			return _this.workerScript;
+		};
+		// This internally has to get the current script, which gets the worker
+		// script from workerscript map as a first step.
+		// getSciptLogs() was chosen because it's 0GB and has no effect.
+		ns.getScriptLogs();
+		// Just in case something goes wrong.
+		Map.prototype.get = orig;
+		if (!_this.workerScript?.print) {
+			// throw, because we have to abort everything **hard**
+			let version = "unknown";
+			try {
+				version = ns.ui.getGameInfo().version;
+			} catch (e) { }
+			throw new Error(
+				`Couldn't hook WorkerScript! Is this the right version of BitBurner? Was expecting 2.3(ish), got ${version}`
+			);
+		}
+		_this.workerScript.print("Successfully hooked WorkerScript!");
+	}
+
+	/** @param {NS} ns */
+	constructor(ns) {
+		ns.printf("Starting...");
+		this.hookWorkerScript(ns);
+		// If a cleanup function was registered before, call it.
+		globalThis.RunInBackgroundCleanupFunc?.();
+		const origReact = this.hookReact();
+		globalThis.RunInBackgroundCleanupFunc = () => {
+			React.createElement = origReact;
+		};
+		this.registerHandler();
+		ns.printf(
+			`Background audio has started up ${this.active ? "" : "in"}active.`
+		);
+		ns.printf("Script will keep functioning in the background.");
+	}
+}
+
+/** @param {NS} ns */
+export function main(ns) {
+	new RunInBackground(ns);
+}
