d34bf468533f83b5168b340f2dc49537f19aa44c
CTNOriginals <info@ctn-originals.com>
2026-01-06 17:56:26 +0100

    MISC: solver logger

--- managers/contract/solvers/AlgorithmicStockTraderI.ts
+++ managers/contract/solvers/AlgorithmicStockTraderI.ts
@@ -1,3 +1,27 @@
-export function Solve(data: Extract<CodingContractObject, { type: 'Algorithmic Stock Trader I' }>['data']): string {
-	return ""
+
+export function Solve(data: Extract<CodingContractObject, { type: 'Algorithmic Stock Trader I' }>['data'], ns?: NS): string {
+	const log = (...msg: any[]) => {
+		if (!ns) {
+			return
+		}
+		ns.print(...msg)
+	}
+
+	const scores: number[] = [0].fill(0, 0, data.length - 1)
+
+	for (let i = 0; i < data.length; i++) {
+		const val = data[i]
+		let best = 0
+
+		for (let j = i + 1; j < data.length; j++) {
+			const profit = data[j] - val
+			if (profit > best) {
+				best = profit
+			}
+		}
+
+		scores[i] = best
+	}
+
+	return Math.max(...scores).toString()
 }
--- managers/contract/solvers/definiton.ts
+++ managers/contract/solvers/definiton.ts
@@ -1,4 +1,10 @@
-// export function Solve(data: Extract<CodingContractObject, { type: 'type_name' }>['data']): string {
-export function Solve(data: CodingContractObject['data']): string {
+// export function Solve(data: Extract<CodingContractObject, { type: 'type_name' }>['data'], ns?: NS): string {
+export function Solve(data: CodingContractObject['data'], ns?: NS): string {
+	const log = (...msg: any[]) => {
+		if (!ns) {
+			return
+		}
+		ns.print(...msg)
+	}
 	return ""
 }
