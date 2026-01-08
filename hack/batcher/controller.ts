483ce5ad1d6946a401d91efe2a6e82933cd691ae
CTNOriginals <info@ctn-originals.com>
2026-01-08 13:18:49 +0100

    CREATE: batcher test

--- hack/batcher/controller.ts
+++ hack/batcher/controller.ts
@@ -4,13 +4,6 @@ import * as Worm from '../worm.ts'
 import { ServerController } from './serverController.ts'
 import * as Data from './data.ts'
 
-
-const batchScripts: Data.TBatchScript = {
-	weaken: 'hack/batcher/scripts/weaken.ts',
-	grow: 'hack/batcher/scripts/grow.ts',
-	hack: 'hack/batcher/scripts/hack.ts',
-}
-
 let ns: NS
 
 export async function main(n: NS) {
@@ -18,54 +11,19 @@ export async function main(n: NS) {
 	ns.disableLog('ALL')
 	const logger = new Logger(ns)
 	const log = (...msg: any[]) => logger.log(...msg)
-	const nf = ns.format.number
 
 	const host = ns.getHostname()
-
 	const controllers: Data.THostMap<ServerController> = {}
 	const hostList: string[] = []
 
 	await Worm.ServerWorm(ns, host, (server: Server) => {
-		// servers.push(server as TServer)
 		controllers[server.hostname] = new ServerController(
 			ns,
 			server.hostname,
 			0.5,
-			batchScripts,
+			Data.BatchScripts,
 		)
 		hostList.push(server.hostname)
 	})
-
-	hostList.sort((a, b) => { return controllers[b].GetScore() - controllers[a].GetScore() })
-
-	const maxspace = 18
-	let totalScore = 0
-	const serverScores: { [host: string]: number } = {}
-
-	for (const host in controllers) {
-		const server = controllers[host]
-		const score = server.GetScore()
-
-		if (server.GetServer().serverGrowth <= 1 || score <= 0) {
-			continue
-		}
-
-		serverScores[server.Target] = score
-		totalScore += score
-	}
-
-	for (const host of hostList) {
-		const server = controllers[host]
-		const score = serverScores[host]
-
-		if (!score) {
-			continue
-		}
-
-		log([
-			`${server.GetInfoString()}`,
-			` \t(${ns.format.percent(((score / totalScore)))})`,
-		].join(''))
-	}
 }
 
---
+++ hack/batcher/test.ts
@@ -0,0 +1,78 @@
+import { Logger } from '../../logging/index.ts'
+import * as Worm from '../worm.ts'
+import { ServerController } from './serverController.ts'
+import * as Data from './data.ts'
+
+const testHosts: string[] = [
+	'phantasy'
+]
+
+export async function main(ns: NS) {
+	ns.disableLog('ALL')
+	const logger = new Logger(ns)
+	const log = (...msg: any[]) => logger.log(...msg)
+
+	const host = ns.getHostname()
+	const controllers: Data.THostMap<ServerController> = {}
+	const hostList: string[] = []
+
+	await Worm.ServerWorm(ns, host, (server: Server) => {
+		if (testHosts.length != 0 && !testHosts.includes(server.hostname)) {
+			return
+		}
+
+		controllers[server.hostname] = new ServerController(
+			ns,
+			server.hostname,
+			0.5,
+			Data.BatchScripts
+		)
+
+		hostList.push(server.hostname)
+	})
+
+	hostList.sort((a, b) => { return controllers[b].GetScore() - controllers[a].GetScore() })
+
+	let totalScore = 0
+	const serverScores: { [host: string]: number } = {}
+
+	for (const host in controllers) {
+		const controller = controllers[host]
+		const score = controller.GetScore()
+
+		if (controller.GetServer().serverGrowth <= 1 || score <= 0) {
+			continue
+		}
+
+		serverScores[controller.Target] = score
+		totalScore += score
+	}
+
+	for (const host of hostList) {
+		const controller = controllers[host]
+		const score = serverScores[host]
+		const server = controller.GetServer()
+
+		if (!score) {
+			continue
+		}
+
+		log([
+			`${controller.GetInfoString()}`,
+			` \t(${ns.format.percent(((score / totalScore)))})`,
+		].join(''))
+
+		await ns.hack(server.hostname)
+
+
+		try {
+			controller.Initialize()
+		} catch (e: any) {
+			log(e)
+		}
+	}
+
+	while (true) {
+		await ns.asleep(100)
+	}
+}
